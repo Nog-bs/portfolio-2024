6ef1e5e3e9876fc0e32039271e264e4bac43ff4b
Edro Gonzales <egonzales15@my.bcit.ca>
2023-12-29 18:49:28 -0800

    feat (wip): add wip for contacts and projects

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -4,7 +4,9 @@ import transition from "./transition";
 const Contact = () => {
     return (
         <div className="flex flex-col items-center justify-center pt-24">
-            <h1>Contact Page: Work in progress...</h1>
+            <h1 className="inline-block px-6 py-2 mt-12 text-white bg-highlight rounded">
+                Work in progress...
+            </h1>
         </div>
     );
 };
