6eb2aa0caf59f7973068d5b666ac6e71d4b504ac
Edro Gonzales <egonzales15@my.bcit.ca>
2024-01-04 18:42:02 -0800

    feat (projects): complete projects section

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,11 +1,21 @@
 import React from "react";
+import GitHubIcon from "@mui/icons-material/GitHub";
+import HappyBonsai from "../assets/images/happy-bonsai.jpg";
 import Portfolio from "../assets/images/portfolio.png";
 import PikachuAdventure from "../assets/images/pikachu-adventure.png";
 import RateVanRent from "../assets/images/rate-van-rent.png";
 import transition from "./transition";
 
-// Sample data for projects
 const projectList = [
+    {
+        title: "Happy Bonsai",
+        imageUrl: HappyBonsai,
+        description:
+            "'Happy Bonsai' is an innovative application I conceptualized and developed during my final days at BrainStation. This unique app combines 3D visualization with book tracking, allowing users to visually monitor their reading progress. As users log books, they see their virtual bonsai grow, making reading progress both fun and visually appealing.",
+        technologies: ["React", "Three.js", "Firebase", "Google Books API"],
+        url: "https://happy-bonsai.netlify.app",
+        githubUrl: "https://github.com/Nog-bs/happy-bonsai",
+    },
     {
         title: "Portfolio",
         imageUrl: Portfolio,
@@ -22,7 +32,6 @@ const projectList = [
         technologies: ["Python", "Pygame", "Unit Testing", "Documentation"],
         url: "https://github.com/Nog-bs/pikachus_adventure",
     },
-    // Add two more projects here
     {
         title: "Rate Van Rent",
         imageUrl: RateVanRent,
@@ -30,41 +39,52 @@ const projectList = [
             "'Rate Van Rent' is a mobile web browser application designed to revolutionize the renting experience in Vancouver. This platform provides a unique perspective on housing options, allowing renters to share and access real experiences, distinct from traditional platforms like Facebook or Craigslist.",
         technologies: ["JavaScript", "HTML", "CSS", "Firebase"],
         url: "https://comp1800-demo-202330-edro.web.app",
+        githubUrl: "https://github.com/Nog-bs/edro-rate-van-rent",
     },
 ];
 
-const ProjectCard = ({ project }) => (
-    <a
-        href={project.url}
-        target="_blank"
-        rel="noreferrer"
-        className="flex flex-col md:flex-row items-center bg-default rounded-lg shadow border-2 border-highlight md:hover:opacity-75 transition duration-500 max-w-[400px] md:max-w-xl my-4"
-    >
-        <img
-            className="w-full h-96 object-cover rounded-t-lg md:h-auto md:w-48 md:rounded-none md:rounded-l-lg"
-            src={project.imageUrl}
-            alt={project.title}
-        />
-        <div className="p-4 flex flex-col justify-between leading-normal">
-            <h5 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-white mb-2">
-                {project.title}
-            </h5>
-            <div className="flex flex-wrap justify-start mb-2">
-                {project.technologies.map((tech) => (
-                    <span
-                        key={tech}
-                        className="px-2 py-1 mr-2 mt-2 text-xs font-semibold text-gray-700 bg-gray-200 rounded-full"
-                    >
-                        {tech}
-                    </span>
-                ))}
+const ProjectCard = ({ project }) => {
+    return (
+        <a
+            href={project.url}
+            target="_blank"
+            rel="noreferrer"
+            className="flex flex-col md:flex-row items-center bg-default rounded-lg shadow border-2 border-highlight hover:border-teal-700 transition duration-500 max-w-[400px] md:max-w-xl my-4"
+        >
+            <img
+                className="w-full h-96 object-cover rounded-t-lg md:h-auto md:w-48 md:rounded-none md:rounded-l-lg"
+                src={project.imageUrl}
+                alt={project.title}
+            />
+            <div className="p-4 flex flex-col justify-between leading-normal">
+                <h5 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-white mb-2">
+                    {project.title}
+                </h5>
+                <div className="flex flex-wrap justify-start mb-2">
+                    {project.technologies.map((tech) => (
+                        <span
+                            key={tech}
+                            className="px-2 py-1 mr-2 mt-2 text-xs font-semibold text-gray-700 bg-gray-200 rounded-full"
+                        >
+                            {tech}
+                        </span>
+                    ))}
+                </div>
+                <p className="text-xs text-gray-700 dark:text-gray-400 mb-3">
+                    {project.description}
+                </p>
+                <a
+                    href={project.githubUrl ?? project.url}
+                    target="_blank"
+                    rel="noreferrer"
+                    className="hover:opacity-75 transition duration-500"
+                >
+                    <GitHubIcon fontSize="large" />
+                </a>
             </div>
-            <p className="text-sm text-gray-700 dark:text-gray-400 mb-3">
-                {project.description}
-            </p>
-        </div>
-    </a>
-);
+        </a>
+    );
+};
 
 const Projects = () => {
     return (
